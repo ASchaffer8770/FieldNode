4c4405a2c86dec34044bd85b5ce99d7cccbb62fe
Alex Schaffer <aschaffer8770@gmail.com>
2026-09-09 11:27:37 -0700

    Add Spring context smoke test

---
+++ edge-server/src/test/java/dev/alexschaffer/fieldnode/FieldNodeApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.alexschaffer.fieldnode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldNodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
